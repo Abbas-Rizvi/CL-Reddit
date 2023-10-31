bf145b7727a5bfc3f2f4c6f63a7dbc1951cfecdc
Abbas R <abbas8rizvi@gmail.com>
2023-10-30 23:48:16 -0400

    added RMI abstract class

---
+++ src/Client/RemoteService.java
@@ -0,0 +1,20 @@
+package Client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RemoteService extends Remote{
+
+    // get posts sorted by score
+    String[] getSortedPosts() throws RemoteException;
+
+    // get posts matching search criteria
+    String[] getSearchedPosts(String searchTerm) throws RemoteException;
+
+    // get posts created by user
+    String[] myPosts(String username) throws RemoteException;
+
+    // delete post
+    int deletePost(String username, int id) throws RemoteException;
+
+}
